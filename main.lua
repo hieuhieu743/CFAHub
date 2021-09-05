3c59f8532f4e0363330e45bd7f6c13e9e4d2d237
hieuhieu743 <anhdaden50@gmail.com>
2021-09-05 21:55:03 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -547,12 +547,15 @@ function library:NewWindow(title)
                 local hovering = false
 
                 Toggle_Button.MouseEnter:Connect(function()
-                    if not focusing then
-                        game.TweenService:Create(Toggle_Button, TweenInfo.new(0.1, Enum.EasingStyle.Linear, Enum.EasingDirection.In), {
-                            BackgroundColor3 = Color3.fromRGB(57, 57, 57)
-                        }):Play()
-                        hovering = true
-                    end
+                    game.TweenService:Create(Toggle_Button, TweenInfo.new(0.1, Enum.EasingStyle.Linear, Enum.EasingDirection.In), {
+                        BackgroundColor3 = Color3.fromRGB(44, 44, 44)
+                    }):Play()
+                end)
+
+                Toggle_Button.MouseLeave:Connect(function()
+                    game.TweenService:Create(Toggle_Button, TweenInfo.new(0.1, Enum.EasingStyle.Linear, Enum.EasingDirection.In), {
+                        BackgroundColor3 = Color3.fromRGB(27, 27, 27)
+                    }):Play()
                 end)
 
                 Toggle_Button.MouseButton1Click:Connect(function()
